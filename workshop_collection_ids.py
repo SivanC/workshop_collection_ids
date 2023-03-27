6cfe0ab17e552b6a2be61d525c98a243abd8721a
BuildTools <unconfigured@null.spigotmc.org>
2023-03-27 18:51:02 -0400

    Refactored logic out of main()
    Moved some logic around to hopefully make more sense, and added a separate function called by main().

--- workshop_collection_ids.py
+++ workshop_collection_ids.py
@@ -12,38 +12,60 @@ def main():
 
     usage: workshop_collection_ids.py [-h] [-f FILE] collection_id [collection_id]...
     """
+
+    # Get item ids for collection
+    item_ids = getItemIds(args.publishedfileids)
+    collection_count = 0
+
+    # format with collection id, quotes, and newlines
+    if args.format:
+        res = "Collection {colID}: \n\"".format(colID=collection_count) +\
+                "\",\n\"".join(item_ids) + "\""
+    # if not formmated, then as comma-separated list
+    else:
+        res = ",".join(item_ids) + "\n"
+    # write to file
+    if args.file:
+        with open(args.file[0], 'w') as f:
+            print(res, file=f)
+    # write to stdout
+    else:
+        print(res)
+
+def getItemIds(collection_ids):
     # json to send to API containing number of collections and IDs for each
     data = {}
-    data['collectioncount'] = len(args.publishedfileids)
+    data['collectioncount'] = len(collection_ids)
     for i in range(data['collectioncount']):
-        data['publishedfileids[{n}]'.format(n=str(i))] = args.publishedfileids[i]
+        data['publishedfileids[{n}]'.format(n=str(i))] = collection_ids[i]
 
     # steam API GetCollectionDetails takes collectioncount and a number of collection ids
     url = "https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/"
     res = requests.post(url, data=data)
+    print(json.loads(res.text))
+    col = json.loads(res.text)['response']['collectiondetails']
 
-    # generate and print list of item ids from collection
-    collections = json.loads(res.text)
-    for collection in collections['response']['collectiondetails']:
-        item_ids = []
-        for item in collection['children']:
-            itemID = item['publishedfileid']
-            item_ids.append("{itemID}".format(itemID=itemID))
-        # format with collection id, quotes, and newlines
-        if args.format:
-            res = "Collection: {colID}: \n\"".format(colID=collection['publishedfileid']) +\
-                  "\",\n\"".join(item_ids) + "\""
-        # format as comma-separated list
+    def loop(collections, item_ids):
+        if not collections:
+            return item_ids
         else:
-            res = ",".join(item_ids) + "\n"
-        # write to file
-        if args.file:
-            with open(args.file[0], 'w') as f:
-                print(res, file=f)
-        # write to stdout
-        else:
-            print(res)
-    return 0
+            collection = collections[0]
+            for item in collection['children']:
+                if 'children' in item.keys():
+                    item_ids.append(loop([item], item_ids))
+                else:
+                    item_ids.append(item['publishedfileid'])
+            return loop(collections[1:], item_ids)
+    item_ids = loop(col, [])
+
+
+
+        # generate and print list of item ids from collection
+        #for collection in collections['response']['collectiondetails']:
+        #    for item in collection['children']:
+        #        item_id = item['publishedfileid']
+        #        item_ids.append(str(item_id))
+    return item_ids
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='workshop_collection_ids',
